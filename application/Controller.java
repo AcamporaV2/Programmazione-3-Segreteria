e8b6cfbb8d1601c8d36ea84e13ee4c03c6073e4e
Acampora <92322511+AcamporaV2@users.noreply.github.com>
2024-02-08 18:09:58 +0100

    Add files via upload

--- application/Controller.java
+++ application/Controller.java
@@ -1,113 +1,158 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Button registrazioneButton;
-    
-    @FXML	
-    private Button indietroButton;
+	 @FXML
+	    private Button segreteria;
+	 
+	 @FXML
+	 	private Button Docente;
+   
 
-    @FXML
-    private Button loginButton;
-    
-    @FXML
-    private BorderPane mainPane;
 
-    @FXML
-    public void initialize() {
-        // Verifica che registrazioneButton non sia nullo prima di chiamare setOnAction
-        if (registrazioneButton != null) {
-            registrazioneButton.setOnAction(event -> handleRegistrazioneButtonClick());
-        } 
-        if (indietroButton != null) {
-        	indietroButton.setOnAction(event -> indietro());
-        }
-        
-        if (loginButton != null) {
-        	loginButton.setOnAction(event -> login());
-        }
-    }
 
+	 @FXML 
+	 private Button registrazioneButton;
+	 
+	 @FXML 
+	 private Button cercaStudente;
+	 
+	 @FXML 
+	 private Button esame;
+	 
     @FXML
-    private void handleRegistrazioneButtonClick() {
+    private void showHomeSegreteria(ActionEvent event) {
         try {
-            // Carica il nuovo file FXML
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("RegistrazioneStudente.fxml"));
-            Parent nuovaPagina = loader.load();
+            // Carica il file FXML "HomeSegreteria.fxml"
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HomeSegreteria.fxml"));
+            Parent root = loader.load();
 
-            // Crea una nuova scena
-            Scene nuovaScena = new Scene(nuovaPagina);
+            // Crea una nuova scena con il contenuto del file FXML
+            Scene scene = new Scene(root);
 
-            // Ottieni lo Stage attuale dalla scena corrente
-            Stage stageAttuale = (Stage) mainPane.getScene().getWindow();
-
-            // Imposta la nuova scena nello stesso Stage
-            stageAttuale.setScene(nuovaScena);
-
+            // Crea un nuovo stage (finestra) e imposta la scena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Home Segreteria");
+            stage.show();
         } catch (IOException e) {
-            System.err.println("Errore durante il caricamento di RegistrazioneStudente.fxml:");
             e.printStackTrace();
         }
     }
-    
-    private void indietro() {
-    	try {
-    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
-    		Parent nuovaPagina = loader.load();
-    		
-    		Scene nuovaScena = new Scene(nuovaPagina);
-    		
-    		Stage stageAttuale = (Stage) mainPane.getScene().getWindow();
-    		
-    		stageAttuale.setScene(nuovaScena);
+    @FXML
+    private void showHomeDocente(ActionEvent event) {
+    	 try {
+             // Carica il file FXML "HomeSegreteria.fxml"
+             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HomeDocente.fxml"));
+             Parent root = loader.load();
 
-    	} catch(IOException e) {
-    		System.err.println("Errore durante il caricamento di Home.fxml:");
-    		e.printStackTrace();
-    	}
+             // Crea una nuova scena con il contenuto del file FXML
+             Scene scene = new Scene(root);
+
+             // Crea un nuovo stage (finestra) e imposta la scena
+             Stage stage = new Stage();
+             stage.setScene(scene);
+             stage.setTitle("Home Docente");
+             stage.show();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }    
+   }
+
+    @FXML
+    private void showHomeStudente(ActionEvent event) {
+    	try {
+            // Carica il file FXML "HomeSegreteria.fxml"
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HomeDocente.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nuova scena con il contenuto del file FXML
+            Scene scene = new Scene(root);
+
+            // Crea un nuovo stage (finestra) e imposta la scena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Home Docente");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
     }
     
-//    private void indietroMenuBar() {
-//    	try {
-//    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
-//    		Parent nuovaPagina = loader.load();
-//    		
-//    		Scene nuovaScena = new Scene(nuovaPagina);
-//    		
-//    		Stage stageAttuale = (Stage) mainPane.getScene().getWindow();
-//    		
-//    		stageAttuale.setScene(nuovaScena);
-//
-//    	} catch(IOException e) {
-//    		System.err.println("Errore durante il caricamento di Home.fxml:");
-//    		e.printStackTrace();
-//    	}
-//    }
-    
-    private void login() {
+    @FXML
+    private void MostraRegistrazione(ActionEvent event) {
     	try {
-    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
-    		Parent nuovaPagina = loader.load();
-    		
-    		Scene nuovaScena = new Scene(nuovaPagina);
-    		
-    		Stage stageAttuale = (Stage) mainPane.getScene().getWindow();
-    		
-    		stageAttuale.setScene(nuovaScena);
+            // Carica il file FXML "HomeSegreteria.fxml"
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RegistrazioneStudente.fxml"));
+            Parent root = loader.load();
 
-    	} catch(IOException e) {
-    		System.err.println("Errore durante il caricamento di Login.fxml:");
-    		e.printStackTrace();
-    	}
+            // Crea una nuova scena con il contenuto del file FXML
+            Scene scene = new Scene(root);
+
+            // Crea un nuovo stage (finestra) e imposta la scena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Inserisci uno studente");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
+    }
+    
+    
+    @FXML
+    private void RicercaStudente(ActionEvent event) {
+    	try {
+            // Carica il file FXML "HomeSegreteria.fxml"
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RicercaStudente.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nuova scena con il contenuto del file FXML
+            Scene scene = new Scene(root);
+
+            // Crea un nuovo stage (finestra) e imposta la scena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Inserisci uno studente");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
+    }
+    
+    @FXML
+    private void MostraEsami(ActionEvent event) {
+    	try {
+            // Carica il file FXML "HomeSegreteria.fxml"
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MostraEsami.fxml"));
+            Parent root = loader.load();
+
+            // Crea una nuova scena con il contenuto del file FXML
+            Scene scene = new Scene(root);
+
+            // Crea un nuovo stage (finestra) e imposta la scena
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Inserisci uno studente");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
+    }
+   
+    
+    @FXML
+    private void ConfermaDatiStudente(ActionEvent event) throws IOException {   
     }
 }
